96c202ae1709c26fd7ba98023972a39c7c8dd54d
Дмитрий Фроленко <orangefrol@gmail.com>
2022-12-13 07:55:46 +0300

    git

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
+from pathlib import Path
 
 with open('requirements.txt','r') as f:
     requirements=f.readlines()
 
+this_directory=Path(__file__).parent
+long_description=(this_directory / 'README.MD').read_text(encoding='utf8')
+print(long_description)
+
+
 setup(
-    name='nanofootball',
+    name='nanofootball-sdk',
     version='0.1.0',
     packages=find_packages(where='src'),
-    long_description='A SDK for working with nanofootball.com',
     package_dir={"": "src"},
-    requires=requirements,
+    install_requires=requirements, # лол без этой строчки он ругался на README.MD
     entry_points={
-    'console_scripts': [
-        'nf = nanofoot.cli:main',
-    ],
-},
+        'console_scripts': [
+            'nf = nanofoot.cli:main',
+        ]
+    },
+    url='https://github.com/thefrol/nanofootball-sdk',
+    long_description=long_description,
+    long_description_content_type='text/markdown; variant=GFM',
+    license="MIT"
 )
